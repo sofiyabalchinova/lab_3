0fc22f83a841bf653b8a816c14d2990c745163c6
sofiyabalchinova <610185@mail.ru>
2022-11-22 14:56:42 +0300

    Initial Commit

---
+++ otrezki/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  otrezki
+//
+//  Created by София Бальчинова on 22.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
